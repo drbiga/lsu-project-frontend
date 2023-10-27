5ea276bbb5c6f6ce01903633ef8a549cd71ce36f
Matheus <bigarellimatheus@gmail.com>
2023-10-27 08:05:16 -0500

    removed env variable to avoid problems with .env file in short-term future

--- src/services/student.ts
+++ src/services/student.ts
@@ -13,17 +13,17 @@ interface NextSeqNumberResponse {
 
 export default class StudentService {
     public async getAllStudentNames(): Promise<string[]> {
-        const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/students`);
+        const response = await axios.get(`http://localhost:8000/students`);
         return response.data;
     }
 
     public async getNextSessionSeqNumber(name: string): Promise<NextSeqNumberResponse> {
-        const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/students/${name}/next_session_seq_number`);
+        const response = await axios.get(`http://localhost:8000/students/${name}/next_session_seq_number`);
         return response.data;
     }
 
     public async startNextSession(name: string): Promise<StartSessionResponse> {
-        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/students/${name}/start_next_session`);
+        const response = await axios.post(`http://localhost:8000/students/${name}/start_next_session`);
         return response.data;
     }
 }
