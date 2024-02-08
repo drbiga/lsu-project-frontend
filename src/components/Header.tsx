b533e608ab39ba950fa647cd04396d9987ed858c
Matheus Costa <drbiga25@gmail.com>
2024-02-08 14:32:59 -0600

    less white

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 export default function Header() {
+    const backgroundColor = '#502E81';
     return (
         <div
             className="d-flex justify-content-start align-items-center ps-5 gap-3"
-            style={{backgroundColor: '#502E81', minHeight: '50px', height: '5vh', color: '#fff'}}
+            style={{backgroundColor, minHeight: '50px', height: '5vh', color: '#fff'}}
         >
-            <img src="lsu-logo.svg" alt="LSU Logo" height="30" />
-            <p>&</p>
-            <img src="rutgers-logo.svg" alt="Rutgers Logo" height="30" />
+            <img style={{backgroundColor}} src="lsu-logo.svg" alt="LSU Logo" height="30" />
+            <p style={{backgroundColor}}>&</p>
+            <img style={{backgroundColor}} src="rutgers-logo.svg" alt="Rutgers Logo" height="30" />
         </div>
     );
 }
