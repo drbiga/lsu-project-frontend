59a3a407f91c9064517254ab7da9845c863af694
Matheus <mcost16@lsu.edu>
2024-04-25 15:16:56 -0500

    fix: spelling

--- src/components/Homework.tsx
+++ src/components/Homework.tsx
@@ -3,7 +3,7 @@ export default function Homework() {
         <>
             <h1>Homework</h1>
             <p>
-            Thank you for anwering the reading and composition section of this session.
+            Thank you for answering the reading and comprehension section of this session.
             Please proceed to your normal homework now in a <b>separate</b> tab.
             </p>
             <a href="https://google.com" target="_blank">Click here to open a new tab and continue working</a>
